5ba519f7769337781929e9c8a8db609865d36c9e
suhvin <89975339+suhvin@users.noreply.github.com>
2023-06-01 20:57:19 +0900

    📝 동영상 추가

--- pages/video/index.tsx
+++ pages/video/index.tsx
@@ -1,24 +1,24 @@
 import { useEffect, useRef } from "react";
 
 const VideoPlayer = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  // const videoRef = useRef<HTMLVideoElement | null>(null);
 
-  useEffect(() => {
-    const video = videoRef.current;
-    if (video && video.play) {
-      video.play();
-    }
+  // useEffect(() => {
+  //   const video = videoRef.current;
+  //   if (video && video.play) {
+  //     video.play();
+  //   }
 
-    return () => {
-      if (video && video.pause) {
-        video.pause();
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (video && video.pause) {
+  //       video.pause();
+  //     }
+  //   };
+  // }, []);
 
   return (
-    <video ref={videoRef} controls>
-      <source src="/asset/common/video2.mp4" type="video/quicktime" />
+    <video controls>
+      <source src="/asset/common/video3.mp4" type="video/mp4" />
     </video>
   );
 };
